0c7bf5eb59daa60c58dba7a6bcc5ded0ca28457a
t4mpz <107772127+t4mpz@users.noreply.github.com>
2022-09-01 12:36:57 -0300

    Updated classes.hpp
    Removed using namespace #cringe
    Fixed imports for linux/unix platforms

--- classes.hpp
+++ classes.hpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <stdlib>
 
-using namespace std;
+// using namespace std;
 
 class Menu{
 	public:
 		int opc = 0;
-		ofstream esc;
-		ifstream le;
-		string escrita;
+		std::ofstream esc;
+		std::ifstream le;
+		std::string escrita;
 		Menu();
 		void escrever();
 		void ler();
-	private:
 };
 
 
 Menu :: Menu (){
 
-	cout << "[1]=Escrever"<< endl <<"[2]=Ler"<< endl << "opc: ";
-	cin >> opc;
-	system("cls");
+	std::cout << "[1]=Escrever"<< std::endl <<"[2]=Ler"<< std::endl << "opc: ";
+	std::cin >> opc;
+	std::system("cls");
 	if (opc == 1){
 		escrever();
 	}else if(opc == 2){
 		ler();
 	}else{
-		cout << "apenas as opcoes a cima";
+		std::cout << "apenas as opcoes a cima";
 	}
 
 
@@ -37,26 +38,26 @@ void Menu :: escrever(){
 
 	esc.open("arc.text" , ofstream :: out | ofstream :: app );
 	while (opc == 1){
-		cout << "email : ";
-		cin >> escrita;
-		esc << "email : " << escrita << endl;
+		std::cout << "email : ";
+		std::cin >> escrita;
+		esc << "email : " << escrita << std::endl;
 
-		cout << "site: ";
-		cin >> escrita;
-		esc << "site: " << escrita << endl;
+		std::cout << "site: ";
+		std::cin >> escrita;
+		esc << "site: " << escrita << std::endl;
 
-		cout << "senha: ";
-		cin >> escrita;
-		esc << "senha: " << escrita << endl;
+		std::cout << "senha: ";
+		std::cin >> escrita;
+		esc << "senha: " << escrita << std::endl;
 
-		cout << "usuario: ";
-		cin >> escrita;
-		esc << "usuario: " << escrita << endl;
+		std::cout << "usuario: ";
+		std::cin >> escrita;
+		esc << "usuario: " << escrita << std::endl;
 
-		esc << "***********************" << endl;
-		cout << "[1]=Repetir " << endl << "[2]=Sair" << endl << "opc:";
-		cin >> opc;
-		system("cls");
+		esc << "***********************" << std::endl;
+		std::cout << "[1]=Repetir " << std::endl << "[2]=Sair" << std::endl << "opc:";
+		std::cin >> opc;
+		std::system("cls");
 	}
 	Menu();
 	esc.close();
@@ -69,7 +70,7 @@ void Menu :: ler(){
 	le.open("arc.text");
 	if (le.is_open()){
 		while(getline(le,escrita)){
-			cout << escrita << endl;
+			std::cout << escrita << std::endl;
 		}
 	}
 
